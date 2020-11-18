88e7a0c0db66f50ae0a283707f4a69b31b122158
younghyun <younghyun317@naver.com>
2020-11-18 20:34:08 +0900

    게시판

--- app/src/main/java/com/example/weather/PostWriteModifyActivity.java
+++ app/src/main/java/com/example/weather/PostWriteModifyActivity.java
@@ -9,6 +9,6 @@ public class PostWriteModifyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_write_modify2);
+        setContentView(R.layout.activity_post_write_modify);
     }
 }
