a6ac9b0f625b06ae7ce6181108cd629a327a39d3
younghyun <younghyun317@naver.com>
2020-11-21 18:37:42 +0900

    button connection

--- app/src/main/java/com/example/weather/JoinActivity2.java
+++ app/src/main/java/com/example/weather/JoinActivity2.java
@@ -2,15 +2,40 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.example.weather.R;
 
 public class JoinActivity2 extends AppCompatActivity {
 
+    EditText edtNickName, edtName, edtTel, edtCertification;
+    CheckBox radiobtnWoman, radiobtnMan;
+    Button btnCertification, btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join2);
+
+        edtNickName = findViewById(R.id.edtNickName);
+        edtName = findViewById(R.id.edtName);
+        edtTel = findViewById(R.id.edtTel);
+        edtCertification = findViewById(R.id.edtCertification);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(JoinActivity2.this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/weather/LoginActivity.java
+++ app/src/main/java/com/example/weather/LoginActivity.java
@@ -2,13 +2,46 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnSignUp, btnForgot, btnNaver, btnFacebook, btnKakao;
+    private EditText edtLoginId, edtLoginPw;
+    private CheckBox chkBoxAutoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtLoginId = findViewById(R.id.edtLoginId);
+        edtLoginPw = findViewById(R.id.edtLoginPw);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(LoginActivity.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(LoginActivity.this, JoinActivity1.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
