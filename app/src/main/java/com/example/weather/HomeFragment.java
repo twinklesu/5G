4ba344ee77346146df2a90a0b58e25354ad9dfb1
eunjungL <dldmswjd322@naver.com>
2020-11-21 15:57:40 +0900

    2020.11.21 urlBuilder edition

--- app/src/main/java/com/example/weather/HomeFragment.java
+++ app/src/main/java/com/example/weather/HomeFragment.java
@@ -119,35 +119,11 @@ public class HomeFragment extends Fragment {
         StringBuilder urlBuilder = new StringBuilder(apiUrl);
         try {
             urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "="+serviceKey);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
             urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); //경도
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
             urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); //위도
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
             urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(dataType, "UTF-8"));	/* 타입 */
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
             urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /* 조회하고싶은 날짜*/
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
             urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(baseTime, "UTF-8")); /* 조회하고싶은 시간 AM 02시부터 3시간*/
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        try {
             urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(numOfRows, "UTF-8"));	/* 한 페이지 결과 수 */
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
