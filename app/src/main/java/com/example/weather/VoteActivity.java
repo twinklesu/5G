225673f6df57de47a378bac2e4264e744bd9bf41
younghyun <younghyun317@naver.com>
2020-11-25 00:06:11 +0900

    설문 팝업 성공

--- app/src/main/java/com/example/weather/VoteActivity.java
+++ app/src/main/java/com/example/weather/VoteActivity.java
@@ -2,6 +2,7 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
-public class VoteActivity extends AppCompatActivity {
+public class VoteActivity extends Activity {
 
     TextView txtText;
 
--- app/src/main/java/com/example/weather/fragment/HomeFragment.java
+++ app/src/main/java/com/example/weather/fragment/HomeFragment.java
@@ -179,8 +179,6 @@ public class HomeFragment extends Fragment {
         });
 
         return rootView;
-
-
     }
 
     public static int TO_GRID = 0;
