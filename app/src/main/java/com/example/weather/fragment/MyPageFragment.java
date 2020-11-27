5d4e0816159fff92e0d788e1946c89be23c1797a
eunjungL <dldmswjd322@gmail.com>
2020-11-28 01:23:05 +0900

    Mypage id setting

--- app/src/main/java/com/example/weather/fragment/MyPageFragment.java
+++ app/src/main/java/com/example/weather/fragment/MyPageFragment.java
@@ -1,22 +1,43 @@
 package com.example.weather.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.weather.R;
 
+import org.w3c.dom.Text;
+
 public class MyPageFragment extends Fragment {
+
+    TextView id;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        id = getActivity().findViewById(R.id.myPage_id);
+
+        id.setText(sharedPreferences.getString("user_id", null));
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_my_page, container, false);
     }
 
