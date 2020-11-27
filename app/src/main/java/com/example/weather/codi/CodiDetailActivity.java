27d048be33bd9ad844dc73f74e3bfec99f02a11d
younghyun <younghyun317@naver.com>
2020-11-28 01:59:08 +0900

    connect button today codi detile2

--- app/src/main/java/com/example/weather/codi/CodiDetailActivity.java
+++ app/src/main/java/com/example/weather/codi/CodiDetailActivity.java
@@ -6,27 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.weather.MainActivity;
 import com.example.weather.R;
 
 public class CodiDetailActivity extends AppCompatActivity {
 
-    Button button;
+    ImageButton imgbtnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codi_detail);
 
-//        button = findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i= new Intent(CodiDetailActivity.this, CodiActivity.class);
-//                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                startActivity(i);
-//            }
-//        });
+        imgbtnBack = findViewById(R.id.imgbtnBack);
+        imgbtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(CodiDetailActivity.this, CodiActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
